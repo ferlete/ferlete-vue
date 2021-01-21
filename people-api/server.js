fcc628f72c57edd34012dbb58e6cd08f14073232
ferlete@gmail.com <GithubVltrvof123!>
2021-01-21 09:22:22 -0400

    Initial commit

---
+++ people-api/server.js
@@ -0,0 +1,15 @@
+/**
+ * arquivo: server.js
+ * descrição: arquivo responsável por toda a configuração e execucação do Back-End ('Employee')
+ * data: 21/01/2021
+ * author: Valter Ferlete <ferlete@gmail.com>
+ */
+
+const app = require('./src/app');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Aplicação sendo executada na porta:', port);
+});
+
